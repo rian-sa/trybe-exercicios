684fefb26970a1932f78471ede46715508495f4b
Rian Alves <riansa0902@gmail.com>
2022-07-21 01:59:17 -0300

    primeiro exercicio javascript

---
+++ fundamentos/bloco-4-JavaScript-e-logica-de-programação/dia-1-introdução-JavaScript/scripts.js
@@ -0,0 +1,13 @@
+const myName = "Rian";
+const birthCity = "São Paulo";
+let birthYear = 2002;
+
+console.log (myName);
+console.log (birthCity);
+console.log (birthYear);
+
+birthYear = 2030;
+birthCity = "Rio de Janeiro";
+
+console.log (birthYear);
+console.log (birthCity);
