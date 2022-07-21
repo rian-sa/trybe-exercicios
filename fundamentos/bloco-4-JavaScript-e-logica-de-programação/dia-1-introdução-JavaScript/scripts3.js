a5af9a861c6f66d0081d4a38c0a89b1448ec5085
Rian Alves <riansa0902@gmail.com>
2022-07-21 02:29:58 -0300

    adiciona exercicio 3 de javascript

---
+++ fundamentos/bloco-4-JavaScript-e-logica-de-programação/dia-1-introdução-JavaScript/scripts3.js
@@ -0,0 +1,11 @@
+const nota = 55;
+
+if (nota >= 80) {
+    console.log ("Parabéns, você foi aprovado(a)!");
+}
+else if  (nota < 80 && nota >= 60) {
+    console.log ("Você está na nossa lista de espera, boa sorte!");
+} 
+else {
+    console.log ("Você foi reprovado(a)")
+}
