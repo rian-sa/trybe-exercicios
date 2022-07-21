b0de884cc13fdd48518b922e16ee6f19ceca0e7a
Rian Alves <riansa0902@gmail.com>
2022-07-21 13:21:22 -0300

    adciona exercicios de operadores do javascript

---
+++ fundamentos/bloco-4-JavaScript-e-logica-de-programação/dia-1-introdução-JavaScript/scripts4.js
@@ -0,0 +1,33 @@
+//Exercícios Operadores
+
+// 1. Operador AND
+
+const currentHour = 12;
+let message = "";
+
+if (currentHour >= 22) {
+    message = "Não deveriamos comer nada, é hora de dormir";
+} else if (currentHour >= 18 && currentHour <= 22) {
+    message = "Rango da noite, vamos jantar :D";
+} else if (currentHour >= 14 && currentHour < 18) {
+    message = "Vamos fazer um bolo pro café da tarde?";
+} else if (currentHour >= 11 && currentHour < 14) {
+    message = "Hora do almoço!!!";
+} else if (currentHour >= 4 && currentHour < 11) {
+    message = "Hmmm, cheiro de café recém passado";
+}
+
+console.log (message);
+
+// 2. Operador OR
+
+let weekDay = "quarta-feira";
+
+if (weekDay === "segunda-feira" || weekDay === "terça-feira" || weekDay === "quarta-feira" || weekDay === "quinta-feira" || weekDay === "sexta-feira") {
+    console.log ("Oba, mais um dia de aprendizado na Trybe");
+} else {
+    console.log ("FINALMENTE, descanso merecido UwU");
+}
+
+
+
